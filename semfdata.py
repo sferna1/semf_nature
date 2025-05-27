edda820c4c3168ddd5840af029a7d69f0d18f40f
Sarah Fernandes <sferna21@students.kennesaw.edu>
2025-05-27 17:25:08 -0400

    Found average values for all epochs in Subject 1's ECG and EMG.

--- semfdata.py
+++ semfdata.py
@@ -14,12 +14,27 @@ from collections import defaultdict
 
 from scipy.linalg.blas import ddot 
 
-#— — — — — — — — — —  ♥  — — — — — — — — — — — — — — — — — — —  ♥  — — — — — — — — — — — — — — — — — — —  ♥  — — — — — — — — — — — — — — — — — — —  ♥  — — — — — — — — — — — — — — — — — — —  ♥  — — — — — — — — — — — — — — — — — — — 
+#— — — — — — — — — —  ♥  — — — — — — — — — — — — — — — — — — —  ♥  — — — — — — — — — — — — — — —  — — —  ♥  — — — — — — — — — — — — — — — — — — —  ♥  — — — — — — — — — — — — — — — — — — —  ♥  — — — — — — — — — — — — — — — — — — — 
+eegpath =fr"C:\Users\sferna21\OneDrive - Kennesaw State University\Desktop\SEMF Project\semfdata\preprocs\preprocessed data\EEG\EEG_1.set"
+eeg_epochs=mne.io.read_epochs_eeglab(eegpath)
 
-epochs=mne.io.read_epochs_eeglab(fr"C:\Users\sferna21\OneDrive - Kennesaw State University\Desktop\SEMF Project\semfdata\preprocs\preprocessed data\EEG\EEG_1.set")
+eeg_data1 = eeg_epochs.get_data() #get all the epochs and store it in data1
+eeg_data1_avg = np.mean(eeg_data1, axis=(1, 2)) #using numpy, find the mean of each epoch, 
+eeg_data1_reshaped = eeg_data1_avg.reshape(eeg_data1_avg.shape[0], 1) #uses the epochs available for each subject and turns the data's shape into a vector
+
+ecgpath =fr"C:\Users\sferna21\OneDrive - Kennesaw State University\Desktop\SEMF Project\semfdata\preprocs\preprocessed data\ECG\ECG_1.set"
+ecg_epochs=mne.io.read_epochs_eeglab(ecgpath)
+
+ecg_data1 = ecg_epochs.get_data() #get all the epochs and store it in data1
+ecg_data1_avg = np.mean(ecg_data1, axis=(1, 2)) #using numpy, find the mean of each epoch, 
+ecg_data1_reshaped = ecg_data1_avg.reshape(ecg_data1_avg.shape[0], 1) #uses the epochs available for each subject and turns the data's shape into a vector
+
+emgpath = fr"C:\Users\sferna21\OneDrive - Kennesaw State University\Desktop\SEMF Project\semfdata\preprocs\preprocessed data\EMG\EMG_1.set"
+emg_epochs=mne.io.read_epochs_eeglab(emgpath)
+
+emg_data1 = emg_epochs.get_data() #get all the epochs and store it in data1
+emg_data1_avg = np.mean(emg_data1, axis=(1, 2)) #using numpy, find the mean of each epoch, 
+emg_data1_reshaped = emg_data1_avg.reshape(emg_data1_avg.shape[0], 1) #uses the epochs available for each subject and turns the data's shape into a vector
 
 
-data1 = epochs.get_data()
-data1_avg = np.mean(data1, axis=(1, 2))
-data1_reshaped = data1_avg.reshape(data1_avg.shape[0], 2)
 stop = 1

dcfe2ec62b594b7f9ea3c1aa635f3cfb8037ed00
Sarah Fernandes <sferna21@students.kennesaw.edu>
2025-08-06 11:12:57 -0400

    Correlation heatmap added.

--- eda_plot.py
+++ eda_plot.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 #to read a .csv
 df = pd.read_csv(r"C:\Users\sferna21\OneDrive - Kennesaw State University\Desktop\SEMF Project\dataframes\avg_array.csv")
-
+df = df.drop('EEG epochs', axis=1)
 #1. make figure
 fig, axes = plt.subplots(3, 1)
 sns.kdeplot(data=df, x = "EEG ch mean", ax = axes[0])
@@ -21,10 +21,19 @@ axes[0].set_title("EEG ch mean dist.")
 axes[1].set_title("EMG ch mean dist.") 
 axes[2].set_title("GSR ch mean dist.") 
 
+ax = sns.pairplot(df, hue = 'EEG event ID', palette='Paired')
+plt.suptitle("Pair Plot of Multimodal Dataset")
+sns.move_legend(
+    ax, "lower center",
+    bbox_to_anchor=(.5, 1), ncol=3, title=None, frameon=False
+)
+
+correlation_matrix = df.corr(numeric_only= True)
+plt.figure(figsize=(8,6))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidth=0.5)
+plt.title("Correlation Heatmap")#no correlations is expected
 
 
-
-
-
-
+plt.tight_layout()
 plt.show()
+stop=1
